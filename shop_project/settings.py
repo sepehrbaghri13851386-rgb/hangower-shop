5585a5aca358dd66ccfae16f927db54964e5709d
sepi_En <sepehrbaghri1385.1386@Email.com>
2026-07-26 08:47:25 -0700

    connect to postgres database

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+    )
 }
 
 
