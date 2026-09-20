94730a2258b6c325351cbb318b73551a158c3d20
sepi_En <sepehrbaghri1385.1386@Email.com>
2026-09-20 13:11:20 -0700

    fix database connection

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -90,7 +90,8 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
+        conn_max_age=0,           # هر درخواست یک اتصال تازه
+        conn_health_checks=True,  # اتصال را قبل از استفاده چک می‌کند
     )
 }
 
